00aed81557b05b007c9b433056d990155c523dc5
alan zhang <syd.alanzhang@gmail.com>
2019-08-24 13:10:48 +1000

    fix lint

--- 1160FindWordsThatCanBeFormedByCharacters.js
+++ 1160FindWordsThatCanBeFormedByCharacters.js
@@ -7,21 +7,21 @@ var countCharacters = function(words, chars) {
     let charArray = chars.split(''),
     result = 0
     for (let i = 0; i < words.length; i++) {
-    	if (checkGoodWord(words[i], charArray.slice())) {
-    		result += words[i].length
+        if (checkGoodWord(words[i], charArray.slice())) {
+            result += words[i].length
     	}
     }
     return result
 };
 
 function checkGoodWord(word, array) {
-	for (let j = 0; j < word.length; j++) {
-		let index = array.indexOf(word[j])
-		if (index !== -1) {
-			array.splice(index, 1)
-		} else {
-			return false
-		}
-	}
-	return true
+    for (let j = 0; j < word.length; j++) {
+    let index = array.indexOf(word[j])
+    if (index !== -1) {
+        array.splice(index, 1)
+    } else {
+        return false
+    }
+}
+    return true
 }
