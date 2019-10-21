493748def2d9b98ddba3f39d238d04b41b4a75d7
alan zhang <syd.alanzhang@gmail.com>
2019-10-21 13:37:08 +1100

    1232 fix lint

--- 1232CheckIfItIsAStraightLine.js
+++ 1232CheckIfItIsAStraightLine.js
@@ -3,14 +3,14 @@
  * @return {boolean}
  */
 var checkStraightLine = function(coordinates) {
-	let slope = undefined
+    let slope = undefined
     for (let i = 0; i < coordinates.length - 1; i ++) {
-    	let currentSlop = (coordinates[i + 1][1] - coordinates[i][1]) / (coordinates[i + 1][0] - coordinates[i][0])
-    	if (slope === undefined) {
-    		slope = currentSlop
-    	} else if (slope != currentSlop) {
-    		return false
-		}
+        let currentSlop = (coordinates[i + 1][1] - coordinates[i][1]) / (coordinates[i + 1][0] - coordinates[i][0])
+        if (slope === undefined) {
+            slope = currentSlop
+        } else if (slope != currentSlop) {
+            return false
+        }
     }
     return true
 };
