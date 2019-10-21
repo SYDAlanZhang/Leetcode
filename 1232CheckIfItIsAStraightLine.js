f7b0b5ea13b6d177d4705ccc454c02780daf0946
alan zhang <syd.alanzhang@gmail.com>
2019-10-21 13:40:57 +1100

    1232 Minor fix

--- 1232CheckIfItIsAStraightLine.js
+++ 1232CheckIfItIsAStraightLine.js
@@ -5,10 +5,10 @@
 var checkStraightLine = function(coordinates) {
     let slope = undefined
     for (let i = 0; i < coordinates.length - 1; i ++) {
-        let currentSlop = (coordinates[i + 1][1] - coordinates[i][1]) / (coordinates[i + 1][0] - coordinates[i][0])
+        let currentSlope = (coordinates[i + 1][1] - coordinates[i][1]) / (coordinates[i + 1][0] - coordinates[i][0])
         if (slope === undefined) {
-            slope = currentSlop
-        } else if (slope != currentSlop) {
+            slope = currentSlope
+        } else if (slope != currentSlope) {
             return false
         }
     }
