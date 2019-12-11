2c0c25c2b841585bd519c094778ca9520963ddb7
alan zhang <syd.alanzhang@gmail.com>
2019-12-12 10:41:08 +1100

    1281 subtract the product and sum of digits of an integer

---
+++ 1281SubtractTheProductAndSumOfDigitsOfAnInteger.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let subtractProductAndSum = function(n) {
+	let sumAdd = 0,
+		sumProduct = 1
+	while (n) {
+		let currentDigit = n % 10
+		sumAdd += currentDigit
+		sumProduct *= currentDigit
+		n = ~~(n / 10)
+	}
+	return sumProduct - sumAdd
+};
+
+console.log(subtractProductAndSum(234))
