ede8f8857ea4164b1c9f5d518175596f1295c81c
alan zhang <syd.alanzhang@gmail.com>
2019-12-12 11:43:03 +1100

    fix linter

--- 1281SubtractTheProductAndSumOfDigitsOfAnInteger.js
+++ 1281SubtractTheProductAndSumOfDigitsOfAnInteger.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 let subtractProductAndSum = function(n) {
-	let sumAdd = 0,
-		sumProduct = 1
-	while (n) {
-		let currentDigit = n % 10
-		sumAdd += currentDigit
-		sumProduct *= currentDigit
-		n = ~~(n / 10)
-	}
-	return sumProduct - sumAdd
+    let sumAdd = 0,
+        sumProduct = 1
+    while (n) {
+        let currentDigit = n % 10
+        sumAdd += currentDigit
+        sumProduct *= currentDigit
+        n = ~~(n / 10)
+    }
+    return sumProduct - sumAdd
 };
 
 console.log(subtractProductAndSum(234))
